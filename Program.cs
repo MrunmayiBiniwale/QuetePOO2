5903b810432277c1f40a095f44a6810ab3e24dd1
Mrunmayi Biniwale <mrunmayibiniwale99@gmail.com>
2024-06-18 14:47:33 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace QuetePOO2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfNumbers = new List<int>();
+            int input = 0;
+            int lengthOfList = 0;
+            int whileCounter = 0;
+            float average = 0;
+
+            Console.WriteLine("How many numbers would you like to add in the list? ");
+            int.TryParse(Console.ReadLine(), out lengthOfList);
+            Console.WriteLine("Please enter numbers one per line :");
+
+            while (whileCounter < lengthOfList) 
+            {        
+                int.TryParse(Console.ReadLine(), out input);
+                listOfNumbers.Add(input);
+                whileCounter++; 
+            }
+
+            Program.DisplayArrayAndAverage(listOfNumbers);
+        }
+
+        public static double CalculateAverage(List<int> listOfNumbers)
+        {
+            double average = 0;
+            average = listOfNumbers.Average();
+            return average;
+        }
+
+        public static void DisplayArrayAndAverage(List<int> listOfNumbers)
+        {
+            double average = 0;
+            string printString = "[ ";
+            average = CalculateAverage(listOfNumbers);
+            Console.WriteLine("\nThe Given Array : \n");
+            for (int i = 0; i < listOfNumbers.Count; i++)
+            {
+                if (i == listOfNumbers.Count - 1)
+                    printString += "" + listOfNumbers[i] + " ]";
+                else
+                    printString += "" + listOfNumbers[i] + ", ";
+            }
+
+            Console.WriteLine(printString);
+            Console.WriteLine("\nAverage : " + average);                
+        }
+    }
+}
